d62b93c020f897ff498096e079bac451898f3bbd
xiexuanzheng123 <349328984@qq.com>
2017-06-27 11:16:04 +0800

    build the enviroment of webpack-redux

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: {
+        hmr: [
+            'webpack-hot-middleware/client?reload=true'
+        ],
+        index: [
+            './index'
+        ]
+    },
+    output: {
+        path: path.join(__dirname, '/build'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    "babel-loader"
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.css', '.json']
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Redux',
+            filename: 'bulid/redux.html',
+            template: './index.html',
+            inject: 'body',
+            hash: true,
+            chunks: ['index']
+        })
+    ]
+}
