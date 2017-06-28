193ba86dc62c0532732866a1e8f50eae41a1c64e
xiexuanzheng123 <349328984@qq.com>
2017-06-28 09:22:01 +0800

    add style

--- counter/components/LabelButtons.js
+++ counter/components/LabelButtons.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import { PropTypes } from 'prop-types'
-// import '../css/bootstrap'
+require ('../css/style')
  
  class LabelButton extends Component {
      render() {
          const { increment, decrement, counter } = this.props; //从pros中导入变量和方法
          return (
              <div>
-                 <label className="label">{counter}</label>
-                 <button className="btn btn-default" onClick = {increment}>ADD</button> 
+                 <label className="label">{counter}</label><br/>
+                 <button className="btnAdd" onClick = {increment}>ADD</button> 
                  <button onClick = {decrement}>DEC</button>
              </div>
          )
